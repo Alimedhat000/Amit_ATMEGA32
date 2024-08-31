a64be059fa93fec3d55d1ab479766c29e10e6451
Alimedhat000 <142531761+Alimedhat000@users.noreply.github.com>
2024-09-01 00:02:53 +0300

    improves in itoa4 and other functions

--- LCD4.c
+++ LCD4.c
@@ -52,17 +52,17 @@ void LCD4_Data(char data) {
 }
 
 void LCD4_DATA_str(char*str) {
-    int i;
-    for (i = 0; str[i] != '\0'; i++) {
-        LCD4_Data(str[i]);
+    while (*str) {
+        LCD4_Data(*str++);
     }
 }
 
 void reverse4(char s[]) {
     int i, j;
     char c;
+    int len = strlen(s);
 
-    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+    for (i = 0, j = len - 1; i < j; i++, j--) {
         c = s[i];
         s[i] = s[j];
         s[j] = c;
@@ -72,20 +72,22 @@ void reverse4(char s[]) {
 void itoa4(int n, char s[]) {
     int i, sign;
 
-    if ((sign = n) < 0) /* record sign */
-        n = -n; /* make n positive */
+    if ((sign = n) < 0) { // record sign
+        n = -n; // make n positive
+    }
     i = 0;
-    do { /* generate digits in reverse order */
-        s[i++] = n % 10 + '0'; /* get next digit */
-    } while ((n /= 10) > 0); /* delete it */
-    if (sign < 0)
+    do { // generate digits in reverse order 
+        s[i++] = n % 10 + '0'; // get next digit
+    } while ((n /= 10) > 0); // delete it
+    if (sign < 0) {
         s[i++] = '-';
+    }
     s[i] = '\0';
     reverse4(s);
 }
 
 void LCD4_Data_int(int value) {
-    char str [11];
+    char str [16];
     itoa4(value, str);
 
     LCD4_DATA_str(str);
