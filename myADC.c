075a58739d73396bf38cfd444a76063aae950cb7
Alimedhat000 <142531761+Alimedhat000@users.noreply.github.com>
2024-09-01 00:05:10 +0300

    added read functions and  comments

--- myADC.c
+++ myADC.c
@@ -1,16 +1,19 @@
 #include "myADC.h"
 
-void init_ADC(char ch,char ref,char freq) {
+void init_ADC(char ch, char ref, char freq) {
 
     ADC_select_ch(ch);
-    
+
     ADC_select_freq(freq);
-    
+
     ADC_select_ref(ref);
 
-    ADC_enable();
-    
+//    ADC_auto_trig_enable();
+
     ADC_enable_int();
+
+    ADC_enable();
+
 }
 
 void ADC_select_ch(char ch) {
@@ -20,10 +23,10 @@ void ADC_select_ch(char ch) {
         ADMUX |= (ch); // set the new channel
     }
 }
-   
+
 void ADC_select_ref(char ref) {
-    ADMUX &= ~((1<<REFS0)|(1<<REFS1));
-    ADMUX |= (ref<<6);
+    ADMUX &= ~((1 << REFS0) | (1 << REFS1));
+    ADMUX |= (ref << 6);
 }
 
 void ADC_select_freq(char freq) {
@@ -31,15 +34,53 @@ void ADC_select_freq(char freq) {
     ADCSRA |= (freq);
 }
 
-void ADC_start_conv(){
-    ADCSRA |= (1<<ADSC);
+void ADC_start_conv() {
+    ADCSRA |= (1 << ADSC);
 }
 
 void ADC_enable() {
-    ADCSRA |= (1<<ADEN);
+    ADCSRA |= (1 << ADEN);
 }
 
-void ADC_enable_int(){
-    ADCSRA |= (1<<ADIE);
-    
+void ADC_enable_int() {
+    ADCSRA |= (1 << ADIE);
+}
+
+void ADC_auto_trig_enable() {
+    ADCSRA |= (1 << ADATE);
+}
+
+void ADC_enable_leftAdj() { // is used when the intervals between values is 
+    ADMUX |= (1 << ADLAR); // significantly large
+}
+
+void ADC_wait_conv() {
+    while (!(ADCSRA & (1 << ADIF)));
+}
+
+int ADC_read() {
+    int data = 0;
+    if (ADMUX & (1 << ADLAR)) {
+        // If ADLAR is set (left adjusted)
+        data = (ADCH << 2); // Take the 8 most significant bits from ADCH
+        data |= (ADCL >> 6); // Take the 2 least significant bits from ADCL
+    } else {
+        // If ADLAR is clear (right adjusted)
+        data = ADCL; // Take the entire ADCL (low byte)
+        data |= (ADCH << 8); // Add the 2 bits from ADCH shifted to their correct positions
+    }
+    return data;
+}
+
+int ADC_read_fast() {
+    int data = 0;
+    if (ADMUX & (1 << ADLAR)) {
+        // If ADLAR is set (left-adjusted) and the changes in the first two bits could be neglected
+        data = (ADCH << 2); // Return the 8 most significant bits directly from ADCH with an error of 20mV
+    } else {
+        // If ADLAR is clear (right-adjusted) if the readings is less than 255
+        data = ADCL; // Return the 8 least significant bits from ADCL
+        char dummy = ADCH; // Read ADCH to complete the reading process
+    }
+    return data;
 }
